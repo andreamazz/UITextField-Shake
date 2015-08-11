195d7df14fff05bf829d4476f65a59bbd6ed8634
King-Wizard <king.wizard.contact@gmail.com>
2015-08-11 20:12:14 +0200

    Added some nullable missing keywords in the method signatures.

--- Source/UITextField+Shake.m
+++ Source/UITextField+Shake.m
@@ -18,7 +18,7 @@
     [self shake:times withDelta:delta completion:nil];
 }
 
-- (void)shake:(int)times withDelta:(CGFloat)delta completion:(void (^)(void))handler {
+- (void)shake:(int)times withDelta:(CGFloat)delta completion:(nullable void (^)(void))handler {
     [self _shake:times direction:1 currentTimes:0 withDelta:delta speed:0.03 shakeDirection:ShakeDirectionHorizontal completion:handler];
 }
 
@@ -34,11 +34,11 @@
     [self shake:times withDelta:delta speed:interval shakeDirection:shakeDirection completion:nil];
 }
 
-- (void)shake:(int)times withDelta:(CGFloat)delta speed:(NSTimeInterval)interval shakeDirection:(ShakeDirection)shakeDirection completion:(void (^)(void))handler {
+- (void)shake:(int)times withDelta:(CGFloat)delta speed:(NSTimeInterval)interval shakeDirection:(ShakeDirection)shakeDirection completion:(nullable void (^)(void))handler {
     [self _shake:times direction:1 currentTimes:0 withDelta:delta speed:interval shakeDirection:shakeDirection completion:handler];
 }
 
-- (void)_shake:(int)times direction:(int)direction currentTimes:(int)current withDelta:(CGFloat)delta speed:(NSTimeInterval)interval shakeDirection:(ShakeDirection)shakeDirection completion:(void (^)())handler {
+- (void)_shake:(int)times direction:(int)direction currentTimes:(int)current withDelta:(CGFloat)delta speed:(NSTimeInterval)interval shakeDirection:(ShakeDirection)shakeDirection completion:(nullable void (^)())handler {
     [UIView animateWithDuration:interval animations:^{
         self.transform = (shakeDirection == ShakeDirectionHorizontal) ? CGAffineTransformMakeTranslation(delta * direction, 0) : CGAffineTransformMakeTranslation(0, delta * direction);
     } completion:^(BOOL finished) {
